470269180b3fa7229aa2305f924316bebc29337d
Aisha Diallo <aisha.diallo@shopify.com>
2019-04-03 15:48:28 -0400

    create homepage and about

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => (
+    <section>
+        <h1>About Me</h1>
+        <p>
+            Welcome to my site
+        </p>
+    </section>
+)
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Index = () => (
+  <section>
+    <h1>Bonjour - Hey, I'm Aisha</h1>
+    <Link href="/about">
+      <a title="About me">About me</a>
+    </Link>
+  </section>
+);
+
+export default Index;
